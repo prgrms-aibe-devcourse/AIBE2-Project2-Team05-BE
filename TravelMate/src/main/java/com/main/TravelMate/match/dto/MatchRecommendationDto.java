b5c9ea83c4de504586d9013f6339f31a5a04e694
Nam Jung Hyun <nam9490@gmail.com>
2025-07-24 17:59:44 +0900

    modify

--- TravelMate/src/main/java/com/main/TravelMate/match/dto/MatchRecommendationDto.java
+++ TravelMate/src/main/java/com/main/TravelMate/match/dto/MatchRecommendationDto.java
@@ -1,19 +1,18 @@
 package com.main.TravelMate.match.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+
 import java.time.LocalDate;
 
-// 매칭 상대를 찾기 위한 결과 DTO
-@Data
+@Getter
 @AllArgsConstructor
-public class MatchResultDto {
+public class MatchRecommendationDto {
     private Long userId;
     private String nickname;
-    private Long travelPlanId;
     private String location;
     private LocalDate startDate;
     private LocalDate endDate;
-    private String interests;
-    private double matchRate; // 매칭률
+    private Long travelPlanId;
+    private int compatibilityScore;
 }
