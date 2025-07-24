e7a84ee4fa294cfa0e4c43adc4d3c4cab71b0dfd
Nam Jung Hyun <nam9490@gmail.com>
2025-07-24 12:30:32 +0900

    modify

--- TravelMate/src/main/java/com/main/TravelMate/plan/repository/TravelPlanRepository.java
+++ TravelMate/src/main/java/com/main/TravelMate/plan/repository/TravelPlanRepository.java
@@ -1,10 +1,18 @@
 package com.main.TravelMate.plan.repository;
 
+import com.main.TravelMate.match.entity.TravelStyle;
 import com.main.TravelMate.plan.entity.TravelPlan;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TravelPlanRepository extends JpaRepository<TravelPlan, Long> {
     List<TravelPlan> findByUserId(Long userId);
+
+    Page<TravelPlan> findCompatibleTravelPlansByStyle(@NotBlank(message = "목적지는 필수입니다") String destination, @NotNull(message = "여행 시작일은 필수입니다") LocalDate startDate, @NotNull(message = "여행 종료일은 필수입니다") LocalDate endDate, Long currentUserId, TravelStyle travelStyle, Pageable pageable);
 }
